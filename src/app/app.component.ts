01ad43cec4b212121e3f6b3024f1ef65d3a5ae97
Margarita Pyrkh <m.pyrh@itransition.com>
2019-04-06 20:07:39 +0300

    Add endpoint for charging from Checkout form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { STRIPE } from '../app.settings';
+import { FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +9,46 @@ import { STRIPE } from '../app.settings';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  openCheckout() {
-    const handler = (<any>window).StripeCheckout.configure({
-      key: STRIPE.PUBLIC_KEY,
-      locale: 'auto',
-      token: (token: any) => {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
-      }
-    });
 
-    handler.open({
+  readonly amountControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  private readonly stripePublishableKey = STRIPE.PUBLIC_KEY;
+
+  private readonly checkoutHandler = (window as any).StripeCheckout.configure({
+    key: this.stripePublishableKey,
+    locale: 'auto',
+    token: (token: any) => {
+      this.sendToken(token);
+    }
+  });
+
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  openCheckout() {
+    this.checkoutHandler.open({
       name: 'Demo name',
       description: 'Demo description',
-      amount: 2000
+      image: '../assets/owl.jpg',
+      amount: this.getFormattedCurrency(),
     });
+  }
+
+  private sendToken(token: string): void {
+    console.log(token);
+    const charge = { amount: this.getFormattedCurrency(), token };
+
+    this.http.post('http://localhost:3020/charge', charge)
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+
+  private getFormattedCurrency(): number {
+    return this.amountControl.value * 100;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   declarations: [
     AppComponent,
